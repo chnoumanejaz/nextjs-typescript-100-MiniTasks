e7dc7026bc81b5eae8fb033fbbb7af1dc0b555a0
chnoumanejaz <noumanejaz92@gmail.com>
2024-01-10 02:28:16 +0500

    [functionality] google and github provider added

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,21 @@
 import Credentials from 'next-auth/providers/credentials';
-
 import type { NextAuthConfig } from 'next-auth';
 import { LoginSchema } from './schemas/loginFormSchema';
 import { getUserByEmail } from './data/user';
 import bcrypt from 'bcryptjs';
+import Google from 'next-auth/providers/google';
+import Github from 'next-auth/providers/github';
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,10 +1,26 @@
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { signIn } from 'next-auth/react';
 import React from 'react';
 
 const Social = () => {
+  const handleClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div>
-      <button className="border bg-sky-400">Login with Google</button>
-      <button className="border bg-sky-400">Login with Github</button>
+      <button
+        className="border bg-sky-400"
+        onClick={() => handleClick('google')}>
+        Login with Google
+      </button>
+      <button
+        className="border bg-sky-400"
+        onClick={() => handleClick('github')}>
+        Login with Github
+      </button>
     </div>
   );
 };
